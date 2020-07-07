6f028c17b604e432be1296c81e04cad9659d881e
shineunji <dmswlckato@gmail.com>
2020-07-07 15:10:14 +0900

    docs:arguments

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-function sayHello() {
-  console.log("Hello");
+function sayHello(name, age) {
+  return `Hello ${name} you are ${age} years old`;
 }
 
-sayHello();
+const greet = sayHello("shin", 27);
+const greet1 = sayHello("kim", 25);
+
+console.log(greet);
+console.log(greet1);
