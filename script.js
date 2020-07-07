6a936fe74bd01b95dd6d15397983f9c9f7fbe128
shineunji <dmswlckato@gmail.com>
2020-07-07 19:02:09 +0900

    docs: add, remove, toggle

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 const title = document.querySelector("#title");
 
-const BASE_COLOR = "rgb(52, 73, 94)";
-const OTHER_COLOR = "#7f8c8d";
+const CLICKED_CLASS = "clicked";
 
 function handleClick() {
-  const currentColor = title.style.color;
-  if (currentColor === BASE_COLOR) {
-    title.style.color = OTHER_COLOR;
-  } else {
-    title.style.color = BASE_COLOR;
-  }
+  //   const hasClass = title.classList.contains(CLICKED_CLASS);
+  //   if (hasClass) {
+  //     title.classList.remove(CLICKED_CLASS);
+  //   } else {
+  //     title.classList.add(CLICKED_CLASS);
+  //   }
+
+  title.classList.toggle(CLICKED_CLASS);
 }
 
 function init() {
-  title.style.color = BASE_COLOR;
   title.addEventListener("click", handleClick);
 }
-
 init();
