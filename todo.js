15648c732e6cb7e6ee51eef319876b4aa1639c46
shineunji <dmswlckato@gmail.com>
2020-07-09 17:43:36 +0900

    docs: delete button click event

--- todo.js
+++ todo.js
@@ -5,7 +5,18 @@ const toDoList = document.querySelector(".js-toDoList");
 const TODOS_LS = "toDos";
 
 // 해야 할 일들을 생성할 때 array 추가
-const toDos = [];
+let toDos = [];
+
+function deleteToDo(event) {
+  const btn = event.target;
+  const li = btn.parentNode;
+  toDoList.removeChild(li);
+  const cleanToDos = toDos.filter(function (toDo) {
+    return toDo.id !== parseInt(li.id);
+  });
+  toDos = cleanToDos;
+  saveToDos();
+}
 
 function saveToDos() {
   localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
@@ -18,6 +29,7 @@ function paintToDo(text) {
   const newId = toDos.length + 1;
 
   delBtn.innerText = "X";
+  delBtn.addEventListener("click", deleteToDo);
   span.innerText = text;
 
   li.appendChild(span);
@@ -43,6 +55,10 @@ function handleSubmit(event) {
 function loadToDos() {
   const loadedToDos = localStorage.getItem(TODOS_LS);
   if (loadedToDos !== null) {
+    const parsedToDos = JSON.parse(loadedToDos);
+    parsedToDos.forEach(function (toDo) {
+      paintToDo(toDo.text);
+    });
   }
 }
 
