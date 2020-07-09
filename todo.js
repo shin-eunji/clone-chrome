86f2e0796eecebe80c3d40818ac6b492cf7c5372
shineunji <dmswlckato@gmail.com>
2020-07-09 15:27:39 +0900

    docs: Todo innerText

--- todo.js
+++ todo.js
@@ -5,13 +5,20 @@ const toDoList = form.querySelector(".js-toDoList");
 const TODOS_LS = "toDos";
 
 function paintToDo(text) {
-  console.log(text);
+  const li = document.createElement("li");
+  const delBtn = document.createElement("button");
+  delBtn.value = "X";
+
+  const span = document.createElement("span");
+  span.innerText = text;
+  li.appendChild;
 }
 
 function handleSubmit(event) {
   event.preventDefault();
   const currentValue = toDoInput.value;
   paintToDo(currentValue);
+  toDoInput.value = "";
 }
 
 function loadToDos() {
