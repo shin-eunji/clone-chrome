da2a5c28afccb99557e98718327b75ebca176265
shineunji <dmswlckato@gmail.com>
2020-07-10 12:20:17 +0900

    docs: Chrome Success

--- weather.js
+++ weather.js
@@ -13,6 +13,7 @@ function getWeather(lat, lng) {
     .then(function (json) {
       const temperature = json.main.temp;
       const place = json.name;
+
       weather.innerText = `${temperature} @ ${place}`;
     });
 }
